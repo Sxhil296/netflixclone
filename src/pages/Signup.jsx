ffd498c770c54a466d09c0cd481e97511c2f249a
Sahil <leviack629@gmail.com>
2022-12-17 13:03:52 +0530

    auth

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-   <>
-     <div className='w-full h-screen'>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/d0a0affb-1c76-4cf0-9d75-eb531f32458c/e81da519-216b-4341-b9e7-92d45398ddc3/IN-en-20221214-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="/" className='hidden sm:block absolute w-full h-full object-cover'/>
+    <>
+      <div className='w-full h-screen'>
+        <img
+          className='hidden sm:block absolute w-full h-full object-cover'
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
+          alt='/'
+        />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-        <div className="fixed w-full px-4 py-24 z-50">
-            <div className='max-w-[450px] h-[650px] mx-auto bg-black/75 text-white'>
-                <div className='max-w-[320px] mx-auto py-16'>
-                    <h1 className='text-3xl font-bold'>Sign Up</h1>
-                    <form className='w-full flex flex-col py-4'>
-                        <input className='p-3 my-2 bg-gray-500 rounded' type="email" placeholder='Email' autoComplete='email' />
-                        <input className='p-3 my-2 bg-gray-500 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
-                        <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
-                        <div className='flex justify-between items-center text-sm text-gray-600'>
+        <div className='fixed w-full px-4 py-24 z-50'>
+          <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+            <div className='max-w-[320px] mx-auto py-16'>
+              <h1 className='text-3xl font-bold'>Sign Up</h1>
+              <form
+                onSubmit={handleSubmit}
+                className='w-full flex flex-col py-4'
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='email'
+                  placeholder='Email'
+                  autoComplete='email'
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='password'
+                  placeholder='Password'
+                  autoComplete='current-password'
+                />
+                <button className='bg-red-600 py-3 my-6 rounded font-bold'>
+                  Sign Up
+                </button>
+                <div className='flex justify-between items-center text-sm text-gray-600'>
                   <p>
                     <input className='mr-2' type='checkbox' />
                     Remember me
@@ -28,13 +65,13 @@ const Signup = () => {
                   </span>{' '}
                   <Link to='/login'>Sign In</Link>
                 </p>
-                    </form>
-                </div>
+              </form>
             </div>
+          </div>
         </div>
-     </div>
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Signup
+export default Signup;
